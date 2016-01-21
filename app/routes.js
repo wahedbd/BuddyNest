208af8891ace58f825a2fac067943ec451ac1e8f
Aziz <vaitalaziz1@users.noreply.github.com>
2016-01-21 11:40:15 +0100

    Update routes.js
    Signup ..update Aziz

--- app/routes.js
+++ app/routes.js
@@ -31,6 +31,16 @@ module.exports = function(app, passport, server) {
         failureRedirect : '/login',
         failureFlash : true
     }));
+    // Signup
+    app.get('/signup', function(request, response) {
+        response.render('signup.html', { message: request.flash('signuperror') });
+    });
+    app.post('/signup', passport.authenticate('signup', {
+        successRedirect : '/user',
+        failureRedirect : '/signup',
+        failureFlash : true
+    }));
+
 
     app.get('/edit', function(request, response) {
         response.render('edit.html', { message: request.flash('updateerror') });
